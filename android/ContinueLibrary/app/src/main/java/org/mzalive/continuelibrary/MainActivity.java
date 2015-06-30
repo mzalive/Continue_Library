b91510377f4d98478385833692585830d64a6559
Matthew Mi <mzalive@gmail.com>
2015-06-30 17:43:53 +0800

    Inset Search Bar

--- android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/MainActivity.java
+++ android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/MainActivity.java
@@ -1,14 +1,22 @@
 package org.mzalive.continuelibrary;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SearchBox search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
-        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         setSupportActionBar(toolbar);
 
@@ -24,13 +32,64 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
 
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        /**
+         * Inset Search Box Configuration
+         */
+        search = (SearchBox) findViewById(R.id.searchbox);
+        Drawable recentIconDrawable = getResources().getDrawable(R.mipmap.ic_restore_black_24dp);
+        recentIconDrawable.mutate().setAlpha(0x42);
+        for(int x = 0; x < 5; x++){
+
+            SearchResult option = new SearchResult("历史记录 " + Integer.toString(x), recentIconDrawable);
+            search.addSearchable(option);
+        }
+        search.setLogoText((String) getResources().getText(R.string.app_name));
+        search.setMenuListener(new SearchBox.MenuListener(){
+
+            @Override
+            public void onMenuClick() {
+                //Hamburger has been clicked
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+
+        });
+        search.setSearchListener(new SearchBox.SearchListener(){
+
+            @Override
+            public void onSearchOpened() {
+                //Use this to tint the screen
+            }
+
+            @Override
+            public void onSearchClosed() {
+                //Use this to un-tint the screen
+            }
+
+            @Override
+            public void onSearchTermChanged() {
+                //React to the search term changing
+                //Called after it has updated results
+            }
+
+            @Override
+            public void onSearch(String searchTerm) {
+                Toast.makeText(MainActivity.this, searchTerm +" Searched", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -39,13 +98,13 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        //if (id == R.id.action_settings) {
             return true;
-        }
+        //}
 
-        return super.onOptionsItemSelected(item);
+        //return super.onOptionsItemSelected(item);
     }
 }
