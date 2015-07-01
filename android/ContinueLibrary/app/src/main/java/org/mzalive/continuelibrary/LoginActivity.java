769d49ce7e691638d443a0935cd0b1d4317f9d74
Trigger <yehongjiang2012@gmail.com>
2015-07-01 17:32:37 +0800

    loginActivity

---
+++ android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/LoginActivity.java
@@ -0,0 +1,14 @@
+package org.mzalive.continuelibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Trigger on 2015/7/1.
+ */
+public class LoginActivity  extends Activity{
+    public void onCreate(Bundle saveInstance){
+        super.onCreate(saveInstance);
+        setContentView(R.layout.activity_login);
+    }
+}
