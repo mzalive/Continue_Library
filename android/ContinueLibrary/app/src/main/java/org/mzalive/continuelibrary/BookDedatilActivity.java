40e31d872b291574071bbe8753f83dd570cad816
Trigger <yehongjiang2012@gmail.com>
2015-07-04 14:26:38 +0800

    书籍详情界面更新

--- android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/BookDedatilActivity.java
+++ android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/BookDedatilActivity.java
@@ -3,6 +3,9 @@ package org.mzalive.continuelibrary;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,24 +18,30 @@ public class BookDedatilActivity extends Activity{
     private ImageView toolbar;
     private ImageView ivBookBlur;
     private ImageView ivBookImage;
-    private TextView tvBookTitle;
-    private TextView tvBookPubInfo;
-    private TextView tvBookContentIndex;
-    private TextView tvBookContent;
+    private TextView  tvBookTitle;
+    private TextView  tvBookPubInfo;
+    private TextView  tvBookContentIndex;
+    private TextView  tvBookContent;
+    private Button    btnAdd;
+    private float     screenDp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_book);
 
-        toolbar = (ImageView)findViewById(R.id.toolBar);
-        ivBookBlur = (ImageView)findViewById(R.id.image_book_blur);
-        ivBookImage = (ImageView)findViewById(R.id.image_book);
-        tvBookTitle = (TextView)findViewById(R.id.text_title);
-        tvBookPubInfo = (TextView)findViewById(R.id.text_publish_info);
-        tvBookContentIndex = (TextView)findViewById(R.id.text_content_index);
-        tvBookContent = (TextView)findViewById(R.id.text_content);
+        toolbar             = (ImageView)findViewById(R.id.toolBar);
+        ivBookBlur          = (ImageView)findViewById(R.id.image_book_blur);
+        ivBookImage         = (ImageView)findViewById(R.id.image_book);
+        tvBookTitle         = (TextView)findViewById(R.id.text_title);
+        tvBookPubInfo       = (TextView)findViewById(R.id.text_publish_info);
+        tvBookContentIndex  = (TextView)findViewById(R.id.text_content_index);
+        tvBookContent       = (TextView)findViewById(R.id.text_content);
+        btnAdd              = (Button)findViewById(R.id.button_add);
 
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        screenDp = dm.widthPixels/dm.density;
     }
 
     private class LoadImage extends AsyncTask<Integer, Integer, String> {
