72791c18049f1f432a50d9d1884ab8c20a6734ce
Matthew Mi <mzalive@gmail.com>
2015-07-06 17:18:17 +0800

    basic search result list with simple Section

--- android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/SearchResultListFragment.java
+++ android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/SearchResultListFragment.java
@@ -13,12 +13,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A fragment class holds a book grid view for all conditions generated with args.
  */
 public class SearchResultListFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+    private SearchResultListRecyclerViewAdapter mAdapter;
 
     public SearchResultListFragment() { }
 
@@ -38,9 +42,23 @@ public class SearchResultListFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_search_result_list, container, false);
 
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.rv_search_result_list);
-
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.setAdapter(new SearchResultListRecyclerViewAdapter(getActivity()));
+
+        mAdapter = new SearchResultListRecyclerViewAdapter(getActivity());
+
+        //This is the code to provide a sectioned list
+        List<SectionedRecyclerViewAdapter.Section> sections = new ArrayList<SectionedRecyclerViewAdapter.Section>();
+        //Sections
+        sections.add(new SectionedRecyclerViewAdapter.Section(0,"Section 1"));
+        sections.add(new SectionedRecyclerViewAdapter.Section(3,"Section 2"));
+        sections.add(new SectionedRecyclerViewAdapter.Section(6,"Section 3"));
+
+        //Add your adapter to the sectionAdapter
+        SectionedRecyclerViewAdapter.Section[] dummy = new SectionedRecyclerViewAdapter.Section[sections.size()];
+        SectionedRecyclerViewAdapter mSectionedAdapter = new SectionedRecyclerViewAdapter(getActivity(),R.layout.search_result_section_header,R.id.text,mAdapter);
+        mSectionedAdapter.setSections(sections.toArray(dummy));
+
+        mRecyclerView.setAdapter(mSectionedAdapter);
 
         return rootView;
     }
