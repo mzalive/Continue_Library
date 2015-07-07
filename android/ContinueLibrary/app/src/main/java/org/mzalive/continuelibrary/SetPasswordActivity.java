43210d356767582c448b48604255d53e257b9ee7
Trigger <yehongjiang2012@gmail.com>
2015-07-07 16:22:27 +0800

    修改了设置密码界面

--- android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/SetPasswordActivity.java
+++ android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/SetPasswordActivity.java
@@ -23,17 +23,9 @@ import org.mzalive.continuelibrary.communication.UserInfo;
  * Created by Trigger on 2015/7/6.
  */
 public class SetPasswordActivity extends Activity{
-    private TextView tvUsername;
-    private TextView tvUsernameContent;
-    private TextView tvOldPassword;
     private EditText etOldPasswordContent;
-    private TextView tvWarningOld;
-    private TextView tvNewPassword;
     private EditText etNewPasswordContent;
-    private TextView tvWarningNew;
-    private TextView tvConfirmPassword;
     private EditText etConfirmPasswordContent;
-    private TextView tvWarningConfirm;
     private Button   btnModify;
 
     @Override
@@ -41,84 +33,27 @@ public class SetPasswordActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_password);
 
-        tvUsername               = (TextView)findViewById(R.id.text_userid);
-        tvUsernameContent        = (TextView)findViewById(R.id.text_userid_content);
-        tvOldPassword            = (TextView)findViewById(R.id.text_old_password);
-        etOldPasswordContent     = (EditText)findViewById(R.id.text_old_password_coontent);
-        tvWarningOld             = (TextView)findViewById(R.id.text_warning_old);
-        tvNewPassword            = (TextView)findViewById(R.id.text_new_password);
+        etOldPasswordContent     = (EditText)findViewById(R.id.text_old_password_content);
         etNewPasswordContent     = (EditText)findViewById(R.id.text_new_password_content);
-        tvWarningNew             = (TextView)findViewById(R.id.text_warning_new);
-        tvConfirmPassword        = (TextView)findViewById(R.id.text_confirm_password);
         etConfirmPasswordContent = (EditText)findViewById(R.id.text_confirm_password_content);
-        tvWarningConfirm         = (TextView)findViewById(R.id.text_warning_confirm);
         btnModify                = (Button)findViewById(R.id.button_modify);
 
-        SharedPreferences sp = getSharedPreferences("UserInfo", MODE_PRIVATE);
-        String username = sp.getString("username", "");
-        tvUsernameContent.setText(username);
+//        SharedPreferences sp = getSharedPreferences("UserInfo", MODE_PRIVATE);
+//        String username = sp.getString("username", "");
+//        tvUsernameContent.setText(username);
 
-        textChange change = new textChange();
-        etOldPasswordContent.addTextChangedListener(change);
-        etNewPasswordContent.addTextChangedListener(change);
-        etConfirmPasswordContent.addTextChangedListener(change);
 
-    }
-    class textChange implements TextWatcher{
-        @Override
-        public void afterTextChanged(Editable arg0){
-
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3){
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence cs, int start, int before, int count){
-            if((!etConfirmPasswordContent.getText().toString().equals(etNewPasswordContent.getText().toString()))){
-                tvWarningConfirm.setText("两次输入的密码不一致，请重新输入");
-            }
-            else{
-                tvWarningConfirm.setText("");
-            }
-            if((etNewPasswordContent.getText().length() < 6) && (etNewPasswordContent.getText().length() > 0)){
-                tvWarningNew.setText("请输入6-16位密码，字母区分大小写");
-            }
-            else{
-                tvWarningNew.setText("");
-            }
-            if((etOldPasswordContent.getText().length() > 0) && (etOldPasswordContent.getText().length() < 6)){
-                tvWarningOld.setText("请输入6-16位密码，字母区分大小写");
-            }
-            else{
-                tvWarningOld.setText("");
-            }
-        }
     }
     public void clickModify(View v){
-        Log.d("click", "modify clicked");
-
-        boolean sign1 = true;
-        boolean sign2 = true;
-        boolean sign3 = true;
-        boolean sign4 = etNewPasswordContent.getText().toString().equals(etConfirmPasswordContent.getText().toString());
-        if(etOldPasswordContent.getText().length() < 1){
-            sign1 = false;
-            tvWarningOld.setText("请输入原始密码！");
+        if(etOldPasswordContent.getText().toString().equals("")){
+            Toast.makeText(SetPasswordActivity.this, "原始密码不能为空！",Toast.LENGTH_SHORT).show();
+            return;
         }
-        if(etNewPasswordContent.getText().length() <1){
-            sign2 = false;
-            tvWarningNew.setText("请输入新密码！");
+        if(!etNewPasswordContent.getText().toString().equals(etConfirmPasswordContent.getText().toString())){
+            Toast.makeText(SetPasswordActivity.this, "两次输入的密码不一致！",Toast.LENGTH_SHORT).show();
+            return;
         }
-        if(etConfirmPasswordContent.getText().length() <1){
-            sign3 = false;
-            tvWarningConfirm.setText("请确认密码！");
-        }
-        if(sign1 && sign2 && sign3 && sign4){
-            new setPassword().execute();
-        }
+        new setPassword().execute();
     }
 
     class setPassword extends AsyncTask<Integer, Integer, String>{
@@ -129,10 +64,7 @@ public class SetPasswordActivity extends Activity{
         protected void onPreExecute(){
             btnModify.setClickable(false);
             SharedPreferences sp = getSharedPreferences("UserInfo", MODE_PRIVATE);
-            String username = sp.getString("username", "");
             userId = sp.getString("userId", "");
-            //测试用的
-//            userId = "2";
             oldPassword = MdEncode.encode(etOldPasswordContent.getText().toString());
             newPassword = MdEncode.encode(etNewPasswordContent.getText().toString());
             Log.d("userId", userId);
