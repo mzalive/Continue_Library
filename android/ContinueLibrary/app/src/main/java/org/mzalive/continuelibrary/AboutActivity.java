c132a9e2357c19248ad85139afb85b9309fb8b57
Matthew Mi <mzalive@gmail.com>
2015-07-08 10:46:06 +0800

    About activity: add toolbar

--- android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/AboutActivity.java
+++ android/ContinueLibrary/app/src/main/java/org/mzalive/continuelibrary/AboutActivity.java
@@ -1,24 +1,41 @@
 package org.mzalive.continuelibrary;
 
-import android.app.Activity;
-import android.graphics.Color;
+
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Trigger on 2015/7/6.
  */
-public class AboutActivity extends Activity {
-    private ImageView ivLogo;
-    private TextView tvVersion;
-    private TextView tvCopyright;
+public class AboutActivity extends AppCompatActivity {
+    private ActionBar actionBar;
 
     @Override
     public void onCreate(Bundle savedInstanceStatus){
         super.onCreate(savedInstanceStatus);
         setContentView(R.layout.activity_about);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_arrow_back_white_24dp);
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
